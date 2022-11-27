6f83baf0fbafdbc5a024f4a76182d091874991d8
Mutaz <mu3tazmu@protonmail.com>
2022-11-27 09:58:57 +0000

    Returned linked list functions to core module.

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ LDFLAGS=`./libs/xml2-config --cflags`
 
 all: csscheck
 
-csscheck: csscheck.o storepaths.o corefuns.o linkedlistfuns.o
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ csscheck.o storepaths.o corefuns.o linkedlistfuns.o `./libs/xml2-config --libs`
+csscheck: csscheck.o storepaths.o corefuns.o
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ csscheck.o storepaths.o corefuns.o `./libs/xml2-config --libs`
 
 csscheck.o: src/csscheck.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ -c src/csscheck.c  `./libs/xml2-config --libs`
@@ -15,8 +15,6 @@ storepaths.o: src/storepaths.c
 	$(CC) $(CFLAGS) -o $@ -c src/storepaths.c
 corefuns.o: src/corefuns.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ -c src/corefuns.c `./libs/xml2-config --libs`
-linkedlistfuns.o: src/linkedlistfuns.c
-	$(CC) $(CFLAGS) -o $@ -c src/linkedlistfuns.c
 
 
 clean:
--- src/corefuns.c
+++ src/corefuns.c
@@ -4,7 +4,6 @@
 #include <libxml/HTMLparser.h>
 #include <regex.h>
 #include "../include/corefuns.h"
-#include "../include/linkedlistfuns.h"
 
 #define REGEX_COUNT (153)
 #define COMMENT_REGEX_INDEX (152)
@@ -15,6 +14,72 @@ struct ParserState {
   int stylenum;
   char *style;
   };
+
+struct elt {
+  char *value;
+  struct elt *next;
+};
+
+typedef struct elt *stack;
+
+
+void stack_push(stack *s, char *str)
+{
+  struct elt *e;
+  e = malloc(sizeof(struct elt));
+  if(e == 0){
+    fprintf(stderr, "Error: An instance of %s was found but storing it failed. Skipping.\n", str);
+    return;
+  }
+  e->value = str;
+  e->next = *s;
+  *s = e;
+}
+
+
+char *stack_pop(stack *s)
+{
+  if(*s == 0){
+    return 0;
+  }
+  char *str;
+  struct elt *e;
+  str = (*s)->value;
+  e = *s;
+  *s = e->next;
+  free(e);
+  return str;
+}
+
+
+//loop through linkedlist and count occurences of each color then nullify pointers inside structs. if > 1 print out. 
+void stack_process(stack *s){
+  char *clr;
+  int clrcnt = 1;
+  struct elt *clrelt;
+
+  while(*s){
+    
+    if((clr = stack_pop(s))){
+      
+      for(clrelt = *s; clrelt != 0; clrelt = clrelt->next){
+	if(clrelt->value && !strcmp(clrelt->value, clr)){
+	  clrcnt += 1;
+	  free(clrelt->value);
+	  clrelt->value = 0;
+	}
+      }
+      
+      if(clrcnt > 1){  
+	printf("%s : %d\n", clr, clrcnt);
+	clrcnt = 1;
+      } 
+
+      free(clr);
+    }  
+  }
+  
+}
 
 
 regex_t *create_regexes(void){
